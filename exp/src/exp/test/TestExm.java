487c9d0c333e9b9fbda0603c8229ad338fa7d6f7
arupmukherjee38 <arupmukherjee38@gmail.com>
2019-11-24 19:53:38 +0530

    second

--- exp/src/exp/test/TestExm.java
+++ exp/src/exp/test/TestExm.java
@@ -13,10 +13,12 @@ public class TestExm {
 		l1.add(7);
 		l1.add(5);
 		l1.add(4);
-		
+		int sum=0;
 		for (int i = 0; i < l1.size(); i++) {
 			System.out.println("element="+l1.get(i));
+			sum=+i;
 		}
+		System.out.println("sum="+sum);
 	}
 
 }
